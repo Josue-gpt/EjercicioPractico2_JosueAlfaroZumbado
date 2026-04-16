6e2b063d44bcfc61b82ba4492c7e3b93e7d1ef34
Josue-gpt <jalfaroz2006@gmail.com>
2026-04-15 18:17:52 -0600

    Commit1
    Se crea el proyecto

---
+++ EjercicioPractico2_JosueAlfaroZumbado/src/main/java/EjercicioPractico2_JosueAlfaroZumbado/EjercicioPractico2JosueAlfaroZumbadoApplication.java
@@ -0,0 +1,13 @@
+package EjercicioPractico2_JosueAlfaroZumbado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioPractico2JosueAlfaroZumbadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioPractico2JosueAlfaroZumbadoApplication.class, args);
+	}
+
+}
---
+++ EjercicioPractico2_JosueAlfaroZumbado/src/test/java/EjercicioPractico2_JosueAlfaroZumbado/EjercicioPractico2JosueAlfaroZumbadoApplicationTests.java
@@ -0,0 +1,13 @@
+package EjercicioPractico2_JosueAlfaroZumbado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioPractico2JosueAlfaroZumbadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
